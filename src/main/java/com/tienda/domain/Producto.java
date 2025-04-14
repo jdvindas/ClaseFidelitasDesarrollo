c3f3ce5c2479f6e59b55304255b07cf76d1d6206
José David Vindas Mora <jdvindas@hotmail.com>
2025-04-13 21:07:03 -0600

    Semana 13

---
+++ src/main/java/com/tienda/domain/Producto.java
@@ -0,0 +1,34 @@
+package com.tienda.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="producto")
+public class Producto implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_producto")
+    private Long idProducto;
+    private Long idCategoria;
+    private String descripcion;
+    private String detalle;
+    private String rutaImagen;
+    private double precio;
+    private int existencias;
+    private boolean activo;
+
+    
+    public Producto() {
+    }
+
+    public Producto(String producto, boolean activo) {
+        this.descripcion = producto;
+        this.activo = activo;
+    }
+}
